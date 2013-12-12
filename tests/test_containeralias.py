f3c368fd1c966e6265a67795456fd03c46eb1f86
Christian Schwede <christian.schwede@enovance.com>
2013-12-12 10:47:48 +0000

    change email in copyright

--- tests/test_containeralias.py
+++ tests/test_containeralias.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2013 Christian Schwede <christian.schwede@enovance.com>
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
